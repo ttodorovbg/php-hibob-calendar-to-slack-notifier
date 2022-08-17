c298c76d0996e2327bf01d006cf909ecc062e46b
Todor Todorov <ttodorov@mi9retail.com>
2022-08-17 20:23:34 +0300

    refactoring 1

---
+++ app/src/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Cal;
+use App\Db;
+
+/**
+ * Undocumented class
+ */
+class Main
+{
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public static function run(): void
+    {
+        Db::init();
+        Cal::init();
+
+        $calendar_results = Cal::fetch();
+        $db_active_users = Db::getActiveUsers();
+
+        $deleted_users = [];
+
+        foreach ($db_active_users as $user) { 
+            if (empty($calendar_results[$user['name']][$user['anniversary']])) {
+                // missing user from calendar
+                $deleted_users[] = $user['id'];
+            } else {
+                $calendar_results[$user['name']][$user['anniversary']] = false;
+            }
+        }
+
+        $added_users = Db::addNewUsers($calendar_results);
+
+        Db::deleteUsers($deleted_users);
+        
+        $data = DB::getDataForSlackNotification(
+            array_merge($added_users, $deleted_users)
+        );
+
+        Db::close();        
+
+        Slack::notify($data);
+        Cal::archive($data);
+    }
+}
